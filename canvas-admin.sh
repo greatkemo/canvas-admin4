7a600a44e0aaa10bb45a422dce6ababfb73c0474
greatkemo <ktaynaz@gmail.com>
2023-04-09 23:22:24 +0300

    fixing csv alignment issue

--- canvas-admin.sh
+++ canvas-admin.sh
@@ -257,7 +257,7 @@ user_search() {
   # Parse the response and create the CSV file
   log "info" "Parsing API response and generating CSV file..."
   echo "\"canvas_user_id\",\"user_id\",\"integration_id\",\"authentication_provider_id\",\"login_id\",\"first_name\",\"last_name\",\"full_name\",\"sortable_name\",\"short_name\",\"email\",\"status\",\"created_by_sis\"" > "$output_file"
-  echo "$response" | jq -r '.[] | [.id, .sis_user_id, .integration_id, .authentication_provider_id, .login_id, .name, .sortable_name, .short_name, .email, .workflow_state, .created_by_sis] | @csv' >> "$output_file"
+  echo "$response" | jq -r '.[] | [.id, .sis_user_id, .integration_id, "", .login_id, "", "", .name, .sortable_name, .short_name, "", "", ""] | @csv' >> "$output_file"
 
   log "info" "User search results saved to: $output_file"
 }
